da7a8ab4833d388f2e411fda7dbd1deaf8215698
Ben Cordero <bencord0@condi.me>
2020-02-04 18:34:09 +0000

    Unparmeterize backend tests

--- tests/cases/test_backend.rs
+++ tests/cases/test_backend.rs
@@ -1,14 +1,10 @@
 use crate::common::*;
 use lal::Backend;
-use parameterized_macro::parameterized;
 use std::{fs, path::Path};
 
-#[parameterized(env_name = {"default", "alpine"})]
-pub fn test_configure_backend(env_name: &str) {
+#[test]
+pub fn test_configure_backend() {
     let state = setup();
-    if !cfg!(feature = "docker") && env_name == "alpine" {
-        return;
-    }
 
     assert_eq!(
         fs::canonicalize(Path::new(&state.backend.get_cache_dir())).unwrap(),
